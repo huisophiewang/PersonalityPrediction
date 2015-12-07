8727a93f8490aac4c4c7fba6dd9b29960bfde2e2
sophiewxh <huiwang8921@gmail.com>
2015-12-07 16:14:18 -0500

    finished gsp

--- freq_pattern_gsp.py
+++ freq_pattern_gsp.py
@@ -1,9 +1,11 @@
 import os
+import operator
 from pprint import pprint
 from edit_dist import get_seqs
 from utilities import CUR_DIR, WIFI_OFF_CAMPUS, WIFI_ID_HOME, WIFI_ALL_LOCS
 
 MIN_SUPPORT = 3
+CUT_TO_LEVEL = 4
 
 def replace_home(seqs, id):
     for i, seq in enumerate(seqs):
@@ -16,56 +18,64 @@ def get_all_locs(seqs):
         for loc in seq:
             all_locs.add(loc)
             
-def test(seqs):
-    fdict = {}
-    for seq in seqs:
-        print seq
-        for loc in WIFI_ALL_LOCS:
-            if loc in seq:
-                if loc in fdict:
-                    fdict[loc] += 1
-                else:
-                    fdict[loc] = 1 
-    pprint(fdict)
+def gsp(seqs, level, flist, freq_pat):
     
-    flist = []         
-    for loc in fdict:
-        if fdict[loc] >= MIN_SUPPORT:
-            flist.append([loc])
-    flist.sort()
-    print flist
+#     print '========'
+#     print "level: %d" % level
+
+    if level > CUT_TO_LEVEL:
+        return freq_pat
     
-    clist = []
-    n = len(flist)
-    for i in range(n):
-        for j in range(n):
-            print flist[i], flist[j]
-            if flist[i][1:] == flist[j][:-1]:
-                cad = []
-                cad.extend(flist[i])
-                cad.append(flist[j][-1])
-                clist.append(cad)
-    pprint(clist)  
+    if level == 1:
+        fdict = {}
+        for seq in seqs:
+            print seq
+            for loc in WIFI_ALL_LOCS:
+                if loc in seq:
+                    if loc in fdict:
+                        fdict[loc] += 1
+                    else:
+                        fdict[loc] = 1 
+
+    else:
+        
+        clist = []
+        n = len(flist)
+        for i in range(n):
+            for j in range(n):
+                #print flist[i], flist[j]
+                if flist[i][1:] == flist[j][:-1]:
+                    cad = []
+                    cad.extend(flist[i])
+                    cad.append(flist[j][-1])
+                    clist.append(cad)
+        
+        # todo: prune before scan all seqs
+        fdict = {}
+        for seq in seqs:
+            for cad in clist:
+                if ','.join(cad) in ','.join(seq):
+                    cad_str = ','.join(cad)
+                    if cad_str in fdict:
+                        fdict[cad_str] += 1
+                    else:
+                        fdict[cad_str] = 1
+    fdict = sorted(fdict.items(), key=operator.itemgetter(1), reverse=True)
+    #pprint(fdict) 
     
-    # todo: prune
+    flist = []              
+    for cad_str, freq in fdict:
+        if freq >= MIN_SUPPORT:
+            freq_pat.append((cad_str, freq))
+            if level == 1:
+                flist.append([cad_str])
+            else:
+                flist.append(cad_str.split(','))                
+    #pprint(freq_pat)
     
-    f2dict = {}
-    for seq in seqs:
-        for cad in clist:
-            if ','.join(cad) in ','.join(seq):
-                cad_str = ','.join(cad)
-                if cad_str in f2dict:
-                    f2dict[cad_str] += 1
-                else:
-                    f2dict[cad_str] = 1
-    
-    f2list = []               
-    for cad_str in f2dict:
-        if f2dict[cad_str] >= MIN_SUPPORT:
-            f2list.append(cad_str.split(','))
-    f2list.sort()
-    pprint(f2list)
-    
+    gsp(seqs, level+1, flist, freq_pat)
+
+
 
 if __name__ == '__main__':
     all_locs = set()
@@ -86,5 +96,8 @@ if __name__ == '__main__':
         
         seqs = get_seqs(fp)
         replace_home(seqs, id)
-        test(seqs)
+        
+        freq_pat = []
+        gsp(seqs, 1, [], freq_pat)
+        pprint(freq_pat)
         
