e7918a46b59f77ce326c80b0db0ca028dacbd891
huiwang <huiwang@ccs.neu.edu>
2016-02-08 22:20:16 -0500

    debug logistic regression

--- logistic_regression.py
+++ logistic_regression.py
@@ -14,12 +14,12 @@ def sigmoid(x, beta):
 
 
 
-def train(train_data, y_col, iterations=100):
-    n = data.shape[0]
+def train(train_data, iterations=3):
+    n = train_data.shape[0]
     
-    train_x = train_data[:, [1,2,3]]
+    train_x = train_data[:, :-1]
     train_x = np.insert(train_x, 0, 1, axis=1)
-    train_y = np.array([train_data[:, y_col]]).transpose() # numpy 1d array transpose, use [[]]
+    train_y = np.array([train_data[:, -1]]).transpose() # numpy 1d array transpose, use [[]]
     prob_y = np.zeros((n, 1))
     
     p = train_x.shape[1]
@@ -30,57 +30,48 @@ def train(train_data, y_col, iterations=100):
     prev_lik = 0.0
     # Newton Ralphon update (matrix form) 
     # http://sites.stat.psu.edu/~jiali/course/stat597e/notes2/logit.pdf
-
-    # w - diagonal matrix of weights 
-    w = np.zeros((n, n))
-    for i in range(n):
-        prob_y[i] = sigmoid(train_x[i], beta)
-        w[i,i] = prob_y[i]
-    #print w
-    print prob_y
-    print train_y - prob_y
-
-    # Hessian Matrix  
-    h = np.dot(np.transpose(train_x), np.dot(w, train_x))      
-    #h -= 0.0001*np.identity(p)
-    #print h
-         
-    # update beta
-#     diff = (train_y-prob_y)
-#     print diff.shape
-#     delta = np.dot(np.transpose(train_x), diff)
-#     print delta
-#     beta += np.dot(inv(h), delta)
-#     print beta
-    
-#     # log likelihood
-#     for i in range(n):
-#         z = np.dot(train_x[i], beta)
-#         lik += train_y[i]*z - math.log(1 + math.exp(z))      
-#     print lik
+    for k in range(iterations):
+        print '----------'
+        print k
+ 
+        # w - diagonal matrix of weights 
+        w = np.zeros((n, n))
+        for i in range(n):
+            prob_y[i] = sigmoid(train_x[i], beta)
+            w[i,i] = prob_y[i]*(1-prob_y[i])
+        print prob_y
+        print w
      
-#     if lik - prev_lik < 0.000001:
-#         break
-#     else:
-#         prev_lik = lik
+        # Hessian Matrix  
+        h = np.dot(np.transpose(train_x), np.dot(w, train_x))      
+        #h -= 0.0001*np.identity(p)
+        print h
+             
+        # update beta
+        delta = np.dot(np.transpose(train_x), (train_y-prob_y))
+        beta += np.dot(inv(h), delta)
+        print beta
+        
+        # log likelihood
+        for i in range(n):
+            z = np.dot(train_x[i], beta)[0]
+            lik += (train_y[i][0]*z - math.log(1 + math.exp(z)))     
+        print lik
+        
+#         print lik - prev_lik
+#         if lik - prev_lik < 0.000001:
+#             break
+#         else:
+#             prev_lik = lik
     
         
         
         
 if __name__ == '__main__':
-    fp = r"data\matrix_data\all_wifi_features.csv"
-    data = np.genfromtxt(fp, delimiter=",", dtype=float, skip_header=1)
-    x_cols = [1,2,3]
-    y_col = 4
-    replace_nan(data, y_col)
-    
-#     fp = r"data.txt"
-#     data = np.genfromtxt(fp, delimiter=" ", dtype=float)
-#     print data
-# #     x_cols = [1,2,3]
-# #     y_col = 4
-    
-    train(data, y_col)
+    fp = r"data\matrix_data\logit\test.csv"
+    data = np.genfromtxt(fp, delimiter=",", dtype=float)
+    print data.shape
+    train(data)
     
     
     
