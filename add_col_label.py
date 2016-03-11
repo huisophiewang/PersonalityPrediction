c9da8de7ab64fe543346d1b9ffb48a11db7f90a1
sophiewxh <huiwang8921@gmail.com>
2016-03-11 16:51:15 -0500

    for plotting map in R

---
+++ add_col_label.py
@@ -0,0 +1,18 @@
+import os
+from utilities import CUR_DIR
+
+output_dir = "C:\Users\Sophie\Documents\Statistics\project\gps_addr"
+input_dir = "C:\Users\Sophie\workspace\Personality\data\gps_osm"
+
+for file in os.listdir(input_dir):
+    if not file.endswith('.csv'):
+        continue
+    
+    id = file.split('.')[0][-2:]
+    fr = open(os.path.join(input_dir, file), 'rU')
+    fw = open(os.path.join(output_dir, file), 'a')
+    fw.write(r"id,time,latitude,longitude,wifi,addr" + '\n')
+    lines = fr.readlines()
+    for line in lines:
+        fw.write(line)
+    fw.close()
