eafdd5dda895b04dcca70572bf949808f85f6c04
sophiewxh <huiwang8921@gmail.com>
2019-01-16 16:06:44 -0500

    stress EMA

--- read_ema.py
+++ read_ema.py
@@ -1,8 +1,8 @@
 import os
 import json
 from datetime import datetime
-import pprint
-pp = pprint.PrettyPrinter(width=100)
+from pprint import pprint
+from operator import itemgetter
 from util import write_feature_to_csv
 
 # questions = ["Dimensions protestors",     #2013-04-24-22:41:36
@@ -18,33 +18,41 @@ count = {id:0 for id in ids}
 #q1 = {id:'NA' for id in ids}
 
 def read_response():
-    id_value = {}
-    for question in questions[0]:
-        print '========================================='
-        print question
-        #dir = r'dataset\EMA\response\%s' % question
-        dir = r'dataset\EMA\response\Stress'
-        for file in os.listdir(dir):
-            id = file.split('.')[0][-2:]
-            fr = open(os.path.join(dir, file))
-            data = json.load(fr)
     
-            if not data:
-                continue
-            print '-------------'
-            print 'ID: ' + id
-            for entry in data:
-                time = datetime.fromtimestamp(entry['resp_time']).strftime('%Y-%m-%d-%H:%M:%S')
-                print time
-                pp.pprint(entry)
-              
-
-
+    dir = os.path.join('dataset', 'EMA', 'response', 'Stress')
+    for file in os.listdir(dir):
         
+        id = file.split('.')[0][-2:]
+        fr = open(os.path.join(dir, file))
+        data = json.load(fr)
+
+        if not data:
+            continue
+        
+        if not id in ['59']:
+            continue
+        
+        print '-------------'
+        print 'ID: ' + id
+        #sorted_entries = []
+        entries = []
+        for entry in data:
+            entries.append(entry)
+        entries = sorted(entries, key=itemgetter('resp_time'))
+        
+        print 'number of responses ' + str(len(entries))
+        
+        for entry in entries:
+            time = datetime.fromtimestamp(entry['resp_time']).strftime('%Y-%m-%d-%H:%M:%S')
+            print 
+            print time
+            print entry
+                
+              
 def read_question():     
     json_data=open(r'dataset\EMA\EMA_definition.json').read()
     data = json.loads(json_data)
-    pp.pprint(data)       
+    pprint(data)       
                                
 #pp.pprint(count)
 #write_feature_to_csv(count, 'reply_count', False)
